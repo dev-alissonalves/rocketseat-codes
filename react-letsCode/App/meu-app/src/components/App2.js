6f6721788df5c13102fd58d1fcf116dcfb606cf7
dev-alissonalves <alissoncc.oliveira@gmail.com>
2022-06-26 01:12:06 -0300

    update at project using react js

---
+++ react-letsCode/App/meu-app/src/components/App2.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class App2 extends React.Component{
+
+  constructor(props){
+    super(props)
+  }  
+
+  render(){
+    return(
+
+    <div className='box'>
+        <div id='title'>
+            {this.props.title}
+        </div>
+
+        <div id='text'>
+            {this.props.text}
+        </div>
+
+        <div id='paragraph'>
+            {this.props.paragraph}
+        </div>
+        
+    </div>
+      
+    );
+  }
+}
+
+export default App2;
--- react-letsCode/App/meu-app/src/index.js
+++ react-letsCode/App/meu-app/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App';
+import App2 from './components/App2';
+import App3 from './components/App3';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App3 
+    
+    />
   </React.StrictMode>
 );
 
