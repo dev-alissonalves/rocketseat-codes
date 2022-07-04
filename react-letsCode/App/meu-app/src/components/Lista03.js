e2353e2345786977f47b7c020d31cec604a92d30
dev-alissonalves <alissoncc.oliveira@gmail.com>
2022-07-04 16:17:52 -0300

    finish react class

---
+++ react-letsCode/App/meu-app/src/components/Lista03.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+class Lista03 extends React.Component{
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            items: 
+            [
+                {id:1, nome: 'item1', completo: true},
+                {id:2, nome: 'item2', completo: true},
+                {id:3, nome: 'item3', completo: false},
+                {id:4, nome: 'item4', completo: true},
+            ]
+        }
+    }
+
+  render(){
+    return(
+        <>
+            <ul>
+            {React.Children.map(this.props.children, child => child.type == Item ? child : null)}
+                {this.state.items.map(
+                    (item) => (
+                        <Item key={item.id} completo={item.completo}>
+                            {item.nome}
+                        </Item>
+                    )
+                )}
+            </ul>
+        </>
+    );
+  }
+}
+
+class Item extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const textDecoration = this.props.completo ? 'line-through' : 'none';
+
+        return(
+            <>
+                <li data-id={this.props.id} style={{textDecoration}}>
+                    {this.props.children}
+                </li>
+            </>
+        );
+    }
+}
+
+export {Lista03, Item};
--- react-letsCode/App/meu-app/src/index.js
+++ react-letsCode/App/meu-app/src/index.js
@@ -6,15 +6,17 @@ import ReactDOM from 'react-dom/client';
 //import App4 from './components/App4';
 //import App5 from './components/App5';
 //import Lista from './components/Lista';
-import Lista02 from './components/Lista02';
+//import Lista02 from './components/Lista02';
+//import { Lista03, Item} from './components/Lista03';
+import FormularioReact from './components/FormularioReact';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Lista02 
-    
-    />
+    <FormularioReact>
+
+    </FormularioReact>
   </React.StrictMode>
 );
 
