f82b291186c3908e795c1749bb449886763a73b8
dev-alissonalves <alissoncc.oliveira@gmail.com>
2022-07-12 16:53:33 -0300

    update discover

---
+++ Rocketseat Discover/CONTENTS/JS/9Switch.js
@@ -0,0 +1,25 @@
+/*
+    SWITCH
+
+    * A condicional switch avalia uma expressão, combinando o valor da expressão para um cláusula case, e executa as instruções associadas ao case.
+
+    EXEMPLO ABAIXO UTILIZANDO SWITCH: 
+*/
+let option = 'ç'
+
+switch(option){
+    case 'a':
+        console.log('Escolhi a opção A')
+        break
+    case 'b':
+        console.log('Escolhi a opção B')
+        break
+    case 'c':
+        console.log('Escolhi a opção C')
+        break
+    case 'd':
+        console.log('Escolhi a opção D')
+    default:
+        console.log('N/D')
+}
+
