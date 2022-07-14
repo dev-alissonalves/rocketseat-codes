7b8d02cc883a2a820676c9fc2c768664de3f9be5
dev-alissonalves <alissoncc.oliveira@gmail.com>
2022-07-14 00:45:06 -0300

    update discover

--- Rocketseat Discover/EXERCÍCIO 04/script.js
+++ Rocketseat Discover/EXERCÍCIO 04/script.js
@@ -15,17 +15,6 @@ let notas = [];
 let sum = 0;
 let media = 0;
 
-function startEnd(op){
-    if(op === 'start' || op === 'START'){
-        document.write(`<h1> ##### --------- SISTEMAS DE NOTAS -------- ##### </h1>`)
-    }
-    else{
-        document.write()
-        document.write(`<h1> ##### --------- FIM DA EXECUÇÃO -------- ##### </h1>`)
-    }  
-}
-
-startEnd('start')
 alert('ATENÇÃO! -> INFORME AS NOTAS NO FORMATO DE 0 A 100')
 
 for(let i = 1; i <= 4; i++){
@@ -35,32 +24,51 @@ for(let i = 1; i <= 4; i++){
 
 media = sum/4
 
+let changeElementH2 = document.querySelector('h2#mean-result')
+let changeElementH3 = document.querySelector('h3#concept')
+
+
 if(media >= 90)
 {
-    document.write(`<h2> A média é: ${(media).toFixed(1)} <h2> <br>`)
-    document.write(`<h3> CONCEITO - (A) </h3>`)
+    changeElementH2.innerHTML = 'A sua média foi: ' + media
+    changeElementH2.style.color = '#228813c6'
+
+    changeElementH3.innerHTML = 'SEU CONCEITO FOI (A) - PARABÉNS!'
+    changeElementH3.style.color = '#228813c6'
+    
 }
 else if (media >= 80 && media < 90)
 {
-    document.write(`<h2> A média é: ${(media).toFixed(1)} <h2> <br>`)
-    document.write(`<h3> CONCEITO - (B) </h3>`)
+    changeElementH2.innerHTML = 'A sua média foi: ' + media
+    changeElementH2.style.color = '#228813c6'
+
+    changeElementH3.innerHTML = 'SEU CONCEITO FOI (B) - PARABÉNS!'
+    changeElementH3.style.color = '#228813c6'
 } 
 else if(media >= 70 && media < 80)
 {
-    document.write(`<h2> A média é: ${(media).toFixed(1)} <h2> <br>`)
-    document.write(`<h3> CONCEITO - (C) </h3>`)
+    changeElementH2.innerHTML = 'A sua média foi: ' + media
+    changeElementH2.style.color = '#ffb62f'
+
+    changeElementH3.innerHTML = 'SEU CONCEITO FOI (C)'
+    changeElementH3.style.color = '#ffb62f'
 }
 else if (media >= 60 && media < 70)
 {
-    document.write(`<h2> A média é: ${(media).toFixed(1)} <h2> <br>`)
-    document.write(`<h3> CONCEITO - (D) </h3>`)
+    changeElementH2.innerHTML = 'A sua média foi: ' + media
+    changeElementH2.style.color = '#ff932f'
+
+    changeElementH3.innerHTML = 'SEU CONCEITO FOI (D)'
+    changeElementH3.style.color = '#ff932f'
 }
 else if(media < 60 && media >= 0)
 {
-    document.write(`<h2> A média é: ${(media).toFixed(1)} <h2> <br>`)
-    document.write(`<h3> CONCEITO - (F) </h3>`)
-}else{
-    document.write(`Nota inválida!`)
-}
+    changeElementH2.innerHTML = 'A sua média foi: ' + media
+    changeElementH2.style.color = '#ff2f2f'
 
-startEnd()
+    changeElementH3.innerHTML = 'SEU CONCEITO FOI (F) - SINTO MUITO!'
+    changeElementH3.style.color = '#ff2f2f'
+}else{
+    changeElementH2.innerHTML = 'OPERAÇÃO INVÁLIDA - ATUALIZE A PÁGINA E TENTE NOVAMENTE'
+    changeElementH2.style.color = '#ff2f2f'
+}
