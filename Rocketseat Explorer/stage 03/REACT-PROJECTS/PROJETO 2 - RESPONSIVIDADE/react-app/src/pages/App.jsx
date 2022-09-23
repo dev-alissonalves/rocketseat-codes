794f97b3bb35b3743ac01d8692ad759b054a83c7
Álisson Alves <alissoncc.oliveira@gmail.com>
2022-09-23 11:26:19 -0300

    project 02 stage 03 - in react

--- Rocketseat Explorer/stage 03/REACT-PROJECTS/PROJETO 2 - RESPONSIVIDADE/react-app/src/pages/App.jsx
+++ Rocketseat Explorer/stage 03/REACT-PROJECTS/PROJETO 2 - RESPONSIVIDADE/react-app/src/pages/App.jsx
@@ -4,17 +4,21 @@ export function App() {
   return (
     <>
       <main>
-        <section id='text-description'>
+        
+        <div>
           <p>NOSSO TRABALHO</p>
           <h1>Fazemos projetos para Mobile e Web</h1>
           <p>Se você tem uma grande ideia inovadora e que vai transformar vidas, coloque em prática agora com a gente.</p>
-          <a href="#">Clique aqui e saiba mais</a>
-        </section>
 
+          <a href="http://">Clique e saiba mais</a>
+        </div>
+        
         <ul>
+
           <li><span>1</span> Experiência de usuário</li>
           <li><span>2</span> Pagamentos no aplicativo</li>
           <li><span>3</span> Suporte 24h todos os dias</li>
+        
         </ul>
 
       </main>
