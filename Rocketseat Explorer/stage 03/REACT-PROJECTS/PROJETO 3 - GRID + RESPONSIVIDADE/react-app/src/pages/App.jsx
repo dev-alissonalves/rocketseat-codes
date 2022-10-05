251e3e8361f1169a7758418c3b3d23b376d88420
Álisson Alves <alissoncc.oliveira@gmail.com>
2022-10-05 00:54:55 -0300

    update stage 03

--- Rocketseat Explorer/stage 03/REACT-PROJECTS/PROJETO 3 - GRID + RESPONSIVIDADE/react-app/src/pages/App.jsx
+++ Rocketseat Explorer/stage 03/REACT-PROJECTS/PROJETO 3 - GRID + RESPONSIVIDADE/react-app/src/pages/App.jsx
@@ -13,22 +13,22 @@ export function App() {
         <main>
 
           <div className="cards">
-            <img src="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+            <img src="https://images.unsplash.com/photo-1581090464777-f3220bbe1b8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
             <h2><span>Design</span> Aplicativos</h2>
           </div>
 
           <div className="cards">
-            <img src="https://images.unsplash.com/photo-1535223289827-42f1e9919769?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
+            <img src="https://images.unsplash.com/photo-1550009158-9ebf69173e03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1501&q=80" alt="" />
             <h2><span>Virtual reality</span> Multiverso</h2>
           </div>
 
           <div className="cards">
-            <img src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+            <img src="https://images.unsplash.com/photo-1498049794561-7780e7231661?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
             <h2><span>IA</span> Robótica</h2>
           </div>
 
           <div className="cards">
-            <img src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="" />
+            <img src="https://images.unsplash.com/photo-1535223289827-42f1e9919769?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
             <h2><span>Programação</span> Tecnologia</h2>
           </div>
 
