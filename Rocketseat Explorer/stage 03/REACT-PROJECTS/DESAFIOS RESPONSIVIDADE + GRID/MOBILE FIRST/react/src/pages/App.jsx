f01004de7f9f39da63e0166ebbb42873555ad6d2
Álisson Alves <alissoncc.oliveira@gmail.com>
2022-10-08 00:55:07 -0300

    challenge grid and responsiveness

---
+++ Rocketseat Explorer/stage 03/REACT-PROJECTS/DESAFIOS RESPONSIVIDADE + GRID/MOBILE FIRST/react/src/pages/App.jsx
@@ -0,0 +1,51 @@
+import logo from '../assets/logo-page.svg';
+import milk1 from '../assets/sorvete1.svg';
+import milk2 from '../assets/sorvete2.svg';
+import milk3 from '../assets/sorvete3.svg';
+import milk4 from '../assets/sorvete4.svg';
+import './styles.css';
+export function App() {
+
+  return (
+    <>
+      <div id='page-mobile'>
+        <header>
+
+          <img src={logo} alt="Logo da página" />
+
+          <div id="title">
+            <h1>Space cream</h1>
+          </div>
+
+        </header>
+
+        <main>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a rhoncus leo. Donec at scelerisque magna. Nullam vitae neque sit amet erat dignissim cursus ac ac lectus. Aliquam erat volutpat. Praesent at quam aliquet, </p>
+
+          <div className="card-group">
+            <div className="card">
+              <img src={milk1} alt="" />
+              <h2>PALETAS</h2>
+            </div>
+
+            <div className="card">
+              <img src={milk2} alt="" />
+              <h2>SHAKES</h2>
+            </div>
+
+            <div className="card">
+              <img src={milk3} alt="" />
+              <h2>MASSAS</h2>
+            </div>
+
+            <div className="card">
+              <img src={milk4} alt="" />
+              <h2>PICOLÉS</h2>
+            </div>
+            
+          </div>
+        </main>
+      </div>
+    </>
+  )
+}
