b26eca416e458c3dfb192d5c72850737400f1835
Álisson Alves <alissoncc.oliveira@gmail.com>
2023-01-14 15:55:59 -0300

    completed project divination game

--- Rocketseat Explorer/stage 05/Jogo da Adivinhacao/script.js
+++ Rocketseat Explorer/stage 05/Jogo da Adivinhacao/script.js
@@ -1,17 +1,40 @@
-const numberRandom = Math.round(Math.random()*10)
-let trials = 1
+// Variables
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTrial = document.querySelector("#btnTrial");
+const btnPlayAgain = document.querySelector("#btnPlayAgain");
+let numberRandom = Math.round(Math.random()*10);
+let trials = 0;
 
-function handleClick(event){
-    event.preventDefault();
-    const inputTrial = Number((document.querySelector("#inputNumber")).value)
-    if(inputTrial == numberRandom) {
-        document.querySelector(".screen1").classList.add("hide")
-        document.querySelector(".screen2").classList.remove("hide")
 
-        document.querySelector("#match").innerText = `Acertou em ${trials} vezes`
-    }else{
-        alert("[ERROU]")
+//Events
+btnTrial.addEventListener("click", handleTryClick);
+btnPlayAgain.addEventListener("click", handleTryAgain);
+
+//function for keyboard navigation
+document.addEventListener("keydown", function(e){
+    if(e.key == "Enter" && screen1.classList.contains('hide')){
+        handleTryAgain();
     }
-
+})
+//Functions of CallBack
+function handleTryClick(event){
+    event.preventDefault();
     trials++;
+    const inputTrial = (document.querySelector("#inputNumber"))
+    if(Number(inputTrial.value) == numberRandom) {
+        toggleScreen();
+        screen2.querySelector("#match").innerText = `Acertou em ${trials} vezes!`
+    }
+    inputTrial.value = "";
+}
+function handleTryAgain(){
+    toggleScreen();
+    trials = 0;
+    numberRandom = Math.round(Math.random()*10);
+}
+//Function for change screen
+function toggleScreen(){
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
 }
