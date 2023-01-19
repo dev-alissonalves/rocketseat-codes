f9a43dbfe7e44ae750974856e3a86d649fdc2c15
Álisson Alves <alissoncc.oliveira@gmail.com>
2023-01-18 23:28:08 -0300

    update project IMC

--- Rocketseat Explorer/stage 05/IMC/script.js
+++ Rocketseat Explorer/stage 05/IMC/script.js
@@ -1,9 +1,16 @@
 const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
-const modalWrapper = document.querySelector(".modal-wrapper")
-const modalMessage = document.querySelector(".modal .title span")
-const modalBtnClose = document.querySelector(".modal button.close")
+
+// Estrutura responsável por controlar os atributos e os estados do Modal
+const Modal = {
+    wrapper: document.querySelector(".modal-wrapper"),
+    message: document.querySelector(".modal .title span"),
+    buttonClose: document.querySelector(".modal button.close"),
+
+    open(){ Modal.wrapper.classList.add("open") },
+    close(){ Modal.wrapper.classList.remove("open") }
+}
 
 //Anonymous function
 form.onsubmit = function (event){
@@ -14,12 +21,12 @@ form.onsubmit = function (event){
     const result = IMC(weight, height)
 
     const message = ` Seu IMC é de ${result}`
-    modalMessage.innerText = message
-    modalWrapper.classList.add("open")
+    Modal.message.innerText = message
+    Modal.open()
 }
 
-modalBtnClose.onclick = () => modalWrapper.classList.remove("open")
+Modal.buttonClose.onclick = () => Modal.close()
 
 function IMC(weight, height){
-    return (weight / ((height / 100) ** 2)).toFixed(2)
+    return (weight / ((height / 100) ** 2)).toFixed(2) 
 }
