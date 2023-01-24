6eccbf71f48d1e9000734cebe9eebeadb494e69e
Álisson Alves <alissoncc.oliveira@gmail.com>
2023-01-23 22:02:42 -0300

    project calculateIMC completed

--- Rocketseat Explorer/stage 05/DESAFIO IMC COM MODAL/js/script.js
+++ Rocketseat Explorer/stage 05/DESAFIO IMC COM MODAL/js/script.js
@@ -1,37 +1,36 @@
 import { Modal } from "./modal.js"
 import { alertError } from "./alert-error.js"
+import { calculateIMC, notANumber } from "./utils.js"
 
 const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
 
-//Anonymous function
+inputHeight.oninput =  () => alertError.close()
+inputWeight.oninput = () =>  alertError.close()
+
+
 form.onsubmit = function (event){
     event.preventDefault();
 
     const weight = inputWeight.value;
     const height = inputHeight.value;
 
-    const showAlertError = notANumber(weight) || notANumber(height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-    if(showAlertError){
+    if(weightOrHeightIsNotANumber){
         alertError.open()
         return;
     }
     
     alertError.close()
 
-    const result = IMC(weight, height)
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
 
-    const message = ` Seu IMC é de ${result}`
+function displayResultMessage(result){
+    const message = `Seu IMC é de ${result}`
     Modal.message.innerText = message
     Modal.open()
-}
-
-function IMC(weight, height){
-    return (weight / ((height / 100) ** 2)).toFixed(2) 
-}
-
-function notANumber(value){
-    return isNaN(value) || value == ""
 }
