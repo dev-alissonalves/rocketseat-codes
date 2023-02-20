0371c4accc00f10e6e36dcdd654bcc8ed12aca76
dev-alissonalves <alissoncc.oliveira@gmail.com>
2023-02-19 23:35:13 -0300

    update repositorys

--- Rocketseat Discover/CONTENTS/Async_Promises/Axios/axios_API.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-    axios.get('https://api.github.com/users/dev-alissonalves')
-    .then(result => {
-    console.log(result.data)
-        });
-}
--- Rocketseat Discover/CONTENTS/Async_Promises/Fetch/teste.js
+++
@@ -1,9 +0,0 @@
-const url = "https://api.mercadolibre.com/sites/MLB/search?q=$computador";
-
-function getData()
-{
-    fetch(url)
-    .then(response => response.json())
-}
-
-let data = getData()
