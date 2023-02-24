c0af2830cf28a521358183a4f6ddd6a885f1c7e7
dev-alissonalves <alissoncc.oliveira@gmail.com>
2023-02-24 08:07:01 -0300

    update project SPA

--- Rocketseat Explorer/stage 06/SPA/script.js
+++ Rocketseat Explorer/stage 06/SPA/script.js
@@ -1,19 +1,27 @@
 const routes = {
-    "/": "./pages/home.html",
-    "/about": "./pages/about.html",
-    "/contato": "./pages/contato.html",
-    404: "./pages/404.html",
-}; 
+    "./": "./SPA/pages/home.html",
+    "/about": "./SPA/pages/about.html",
+    "/contato": "./SPA/pages/contato.html",
+    404: "./SPA/pages/404.html",
+};
 
 function route(event) {
     event = event || window.event;
     event.preventDefault();
-    window.history.pushState({}, "", event.target.href)
-    handle()
+    window.history.pushState({}, "", event.target.href);
+    handle();
+}
+// ou pode ser feito assim const { pathname } = window.location
+function handle() {
+    const pathname = window.location.pathname;
+    const route = routes[pathname] || routes[404]
+    fetch(route)
+    .then(data => data.text())
+    .then(html => {
+        document.querySelector('#app').innerHTML = html
+    })
+    
+    console.log(route);
 }
 
-function handle(){
-    const pathname = window.location.pathname
-    // ou pode ser feito assim const { pathname } = window.location
-    console.log(pathname)
-}
+handle()
