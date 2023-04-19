7709cec41969786b7ad190ee5433c74e0008b123
dev-alissonalves <alissoncc.oliveira@gmail.com>
2023-04-19 16:08:06 -0300

    SPA Universe completed

--- Rocketseat Explorer/stage 06/Project Universe SPA/js/index.js
+++ Rocketseat Explorer/stage 06/Project Universe SPA/js/index.js
@@ -1,32 +1,38 @@
 const wallPaper = document.querySelector("body");
 const routes = {
-    "/": "../pages/home/home.html",
-    "/universo": "../pages/universo/universo.html",
-    "/explorar": "../pages/explorar/explorar.html",
-    404: "/pages/404.html",
+  "/": "../pages/home/home.html",
+  "/universo": "../pages/universo/universo.html",
+  "/explorar": "../pages/explorar/explorar.html",
+  404: "/pages/404.html",
 };
 
 function route(event) {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handle();
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handle();
 }
 
 function handle() {
-    const pathname = window.location.pathname;
-    // pegando uma rota - o pathname inicial é o "/" caso ele não encontre mostra o 404
-    const route = routes[pathname] || routes[404];
-    if (route == "../pages/universo/universo.html") {
-        wallPaper.style.backgroundImage = "../images/img-universo.png"
-        console.log(wallPaper)
-    }
-    //função data.text() retorna o conteúdo html
-    fetch(route)
-        .then((data) => data.text())
-        .then((html) => {
-            document.querySelector("#app").innerHTML = html;
-        });
+  const { pathname } = window.location;
+  // pegando uma rota - o pathname inicial é o "/" caso ele não encontre mostra o 404
+  const route = routes[pathname] || routes[404];
+  changeBG(pathname);
+  fetch(route)
+    .then((data) => data.text())
+    .then((html) => {
+      document.querySelector("#app").innerHTML = html;
+    });
+}
+
+function changeBG(pathname) {
+  if (pathname == "/universo") {
+    wallPaper.className = "universo";
+  } else if (pathname == "/explorar") {
+    wallPaper.className = "explorar";
+  }else{
+    wallPaper.className = "home";
+  }
 }
 
 handle();
