23d3c5e9cb3bf0c43969382ff5adc7859e70cf80
dev-alissonalves <alissoncc.oliveira@gmail.com>
2023-04-24 00:20:38 -0300

    created other methods http with express and JS

--- Rocketseat Discover/CONTENTS/Backend APIs/backend/server.js
+++ Rocketseat Discover/CONTENTS/Backend APIs/backend/server.js
@@ -5,4 +5,28 @@ app.listen("3333", () => {
   console.log("Servidor rodando na porta: 3333!");
 });
 
+app.use(express.json());
 
+let content = "";
+console.log(content);
+
+app
+  .route("/")
+  .get((request, response) => {
+    response.send(content);
+  })
+  .post((request, response) => {
+    content = request.body;
+    console.log(content);
+    response.send(content);
+  })
+  .put((request, response) => {
+    content = request.body;
+    console.log(content);
+    response.send(content);
+  })
+  .delete((request, response) => {
+    content = ""
+    response.send(content);
+    console.log(content);
+  });
